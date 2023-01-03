30db0c08d85e67904b486ca359e2e40e776af7cd
Asrat23 <asratendale23@gmail.com>
2023-01-03 08:45:34 -0800

    'front'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 function App() {
+  const [data,setdata]=useState([])
+
+
+
+  
+  const fetchdata=()=>{
+    axios.get("http://127.0.0.1:8000/studlist/").then((response)=>
+     setdata(response.data)).catch((err)=>console.log(err))
+  }
+  
+  
+  
+  useEffect(()=>{fetchdata()},[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+      {
+        data.map(data=>{
+          return <div>
+            <p>First Name  :  {data.firstName}</p>
+            <p>Last Name   :   {data.lastName}</p>
+            <p>Grade       : {data.grade}</p>
+          </div>
+        })
+
+      }
     </div>
+  
   );
 }
 
